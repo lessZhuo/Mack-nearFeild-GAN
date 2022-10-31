d231d3a103addbd090fff99bd983f296f6270649
lessZhuo <799551000@qq.com>
2022-10-31 17:45:16 +0800

    修改cycleGan的读取数据

--- CycleGAN/datasets.py
+++ CycleGAN/datasets.py
@@ -1,67 +1,54 @@
 import glob
-import lzma
-import random
 import os
-
 import numpy as np
-import pandas as pd
-import torch
 from torch.utils.data import Dataset
-from PIL import Image
 import torchvision.transforms as transforms
 
 
-def to_rgb(image):
-    rgb_image = Image.new("RGB", image.size)
-    rgb_image.paste(image)
-    return rgb_image
-
-
 class ImageDataset(Dataset):
-    def __init__(self, root, transforms_=None, unaligned=False, mode="train"):
+    def __init__(self, root, transforms_=None, mode="train", combine=False, direction="x", part="real"):
         self.transform = transforms.Compose(transforms_)
-        self.unaligned = unaligned
+        self.combine = combine
+        self.direction = direction
+        self.part = part
         # 获取该文件夹所有符合模式匹配格式的文件，变成list返回
         self.files_A = sorted(glob.glob(os.path.join(root, "%s/A" % mode) + "/*.*.*"))
         self.files_B = sorted(glob.glob(os.path.join(root, "%s/B" % mode) + "/*.*.*"))
 
     def __getitem__(self, index):
-        matrix_A = pd.read_csv(self.files_A[index % len(self.files_A)], header=None)
-        result_A = np.array(matrix_A)  # 生成np 的array
-        # image_A = Image.fromarray(np.uint8(result_A*255))  # 从数据，生成image对象
+        # 1.读取mask的数据
+        mask = np.load(self.files_A[index % len(self.files_A)])
+        mask = mask[np.newaxis, :, :]
+        mask = mask.astype(np.float)
+        mask = self.transform(mask)
 
-        # image_A = Image.open(self.files_A[index % len(self.files_A)])
+        # 2.读取近场数据
+        near_field = np.load(self.files_B[index % len(self.files_B)])
 
-        if self.unaligned:
-            # image_B = Image.open(self.files_B[random.randint(0, len(self.files_B) - 1)])
-            matrix_B = pd.read_csv(self.files_B[random.randint(0, len(self.files_B) - 1)],
-                                   header=None)
-            result_B = np.array(matrix_B)  # 生成np 的array
-            # image_B = Image.fromarray(np.uint8(result_B*255))  # 从数据，生成image对象
+        # 2.1判读读取xx方向还是yy方向数据
+        if self.direction == "x":
+            nf_real = near_field["xx_real"]
+            nf_imag = near_field["xx_imag"]
         else:
-            # image_B = Image.open(self.files_B[index % len(self.files_B)])
-            matrix_B = pd.read_csv(self.files_B[index % len(self.files_B)], header=None)
-            result_B = np.array(matrix_B)  # 生成np 的array
-            # image_B = Image.fromarray(np.uint8(result_B*255))  # 从数据，生成image对象
+            nf_real = near_field["yy_real"]
+            nf_imag = near_field["yy_imag"]
 
-        # Convert grayscale images to rgb
-        # if image_A.mode != "RGB":
-        #     image_A = to_rgb(image_A)
-        # if image_B.mode != "RGB":
-        #     image_B = to_rgb(image_B)
-        result_A = result_A[:,:,np.newaxis]
-        result_B = result_B[:,:,np.newaxis]
-        # print(result_A.shape)
-        # print(result_B.shape)
-        result_A = result_A.astype(np.float)
-        result_B = result_B.astype(np.float)
-        item_A = self.transform(result_A)
-        item_B = self.transform(result_B)
-        item_A=item_A.type(torch.FloatTensor)
-        item_B = item_B.type(torch.FloatTensor)
-        # print(item_A.shape)
-        # print(item_B.shape)
-        return {"A": item_A, "B": item_B}
+        # 2.2进行类型转换和维度扩充
+        nf_real = nf_real.astype(np.float)
+        nf_imag = nf_imag.astype(np.float)
+        nf_real = nf_real[np.newaxis, :, :]
+        nf_imag = nf_imag[np.newaxis, :, :]
+
+        # 2.3根据是否合并或者读取实部虚部进行返回数据
+        if self.combine:
+            nf = np.concatenate((nf_real, nf_imag), axis=0)
+            nf = self.transform(nf)
+            return {"A": mask, "B": nf}
+        else:
+            if self.part=="real":
+                return {"A": mask, "B": self.transform(nf_real)}
+            else:
+                return {"A": mask, "B": self.transform(nf_imag)}
 
     def __len__(self):
         return max(len(self.files_A), len(self.files_B))
